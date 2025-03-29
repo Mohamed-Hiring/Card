fb35d04e00b650b555fc4bf15384306e8583628a
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-03-29 11:47:22 +0300

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,1 +1,13 @@
-// UI components will go here.
+// button.tsx
+
+import React from 'react';
+
+const Button: React.FC = ({ children, ...props }) => {
+  return (
+    <button {...props} className="btn">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
