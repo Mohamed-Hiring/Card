c8c18a3e08ae8eaa80858d565660b1580ad3e8d9
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-03-29 12:24:42 +0300

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,13 +1,9 @@
 import { ReactNode } from "react";
 
-export function Card({ children }: { children: ReactNode }) {
+export const Card = ({ children }: { children: ReactNode }) => {
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden">
       {children}
     </div>
   );
-}
-
-export function CardContent({ children }: { children: ReactNode }) {
-  return <div className="p-4">{children}</div>;
-}
+};
