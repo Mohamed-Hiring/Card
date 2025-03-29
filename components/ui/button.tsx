3b707cbb8b829dcd5d626a31313ee5ffa9e192df
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-03-29 12:10:05 +0300

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,13 +1,8 @@
 // button.tsx
-
 import React from 'react';
 
-const Button: React.FC = ({ children, ...props }) => {
-  return (
-    <button {...props} className="btn">
-      {children}
-    </button>
-  );
+const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+  return <button {...props} />;
 };
 
 export default Button;
